1374c3b83e8752ac63ab3ce9790e3f1d059a36da
Giginio <gigi.margarito1@gmail.com>
2024-05-19 19:14:26 +0200

    CounterApp

--- server.js
+++ server.js
@@ -8,8 +8,27 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// Counter
+let counter = 0;
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Route to get the current counter value
+app.get('/counter', (req, res) => {
+    res.json({ counter });
+});
+
+// Route to increment the counter
+app.post('/increment', (req, res) => {
+    counter++;
+    res.json({ counter });
+});
+
+// Default route to serve the HTML page
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps Course! (no more smiley faces :()');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(PORT, HOST);
